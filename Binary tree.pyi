9afbaea2a6043e6a3f8188a52f806dde48a937f9
gruhasatya <66037927+gruhasatya@users.noreply.github.com>
2022-05-31 04:22:15 -0700

    commit

--- Binary tree.pyi
+++ Binary tree.pyi
@@ -80,3 +80,163 @@ root.left.right = Node(5)
 print(diameter(root))
 
 
+
+
+
+
+
+
+
+
+
+# Python program to insert element in binary tree
+class newNode():
+
+    def __init__(self, data):
+        self.key = data
+        self.left = None
+        self.right = None
+
+
+""" Inorder traversal of a binary tree"""
+
+
+def inorder(temp):
+    if (not temp):
+        return
+
+    inorder(temp.left)
+    print(temp.key, end=" ")
+    inorder(temp.right)
+
+
+"""function to insert element in binary tree """
+
+
+def insert(temp, key):
+    if not temp:
+        root = newNode(key)
+        return
+    q = []
+    q.append(temp)
+
+    # Do level order traversal until we find
+    # an empty place.
+    while (len(q)):
+        temp = q[0]
+        q.pop(0)
+
+        if (not temp.left):
+            temp.left = newNode(key)
+            break
+        else:
+            q.append(temp.left)
+
+        if (not temp.right):
+            temp.right = newNode(key)
+            break
+        else:
+            q.append(temp.right)
+
+
+# Driver code
+if __name__ == '__main__':
+    root = newNode(10)
+    root.left = newNode(11)
+    root.left.left = newNode(7)
+    root.right = newNode(9)
+    root.right.left = newNode(15)
+    root.right.right = newNode(8)
+
+    print("Inorder traversal before insertion:", end=" ")
+    inorder(root)
+
+    key = 12
+    insert(root, key)
+
+    print()
+    print("Inorder traversal after insertion:", end=" ")
+    inorder(root)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python program to for tree traversals
+
+# A class that represents an individual node in a
+# Binary Tree
+
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+# A function to do inorder tree traversal
+def printInorder(root):
+    if root:
+        # First recur on left child
+        printInorder(root.left)
+
+        # then print the data of node
+        print(root.val),
+
+        # now recur on right child
+        printInorder(root.right)
+
+
+# A function to do postorder tree traversal
+def printPostorder(root):
+    if root:
+        # First recur on left child
+        printPostorder(root.left)
+
+        # the recur on right child
+        printPostorder(root.right)
+
+        # now print the data of node
+        print(root.val),
+
+
+# A function to do preorder tree traversal
+def printPreorder(root):
+    if root:
+        # First print the data of node
+        print(root.val),
+
+        # Then recur on left child
+        printPreorder(root.left)
+
+        # Finally recur on right child
+        printPreorder(root.right)
+
+
+# Driver code
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+print
+"Preorder traversal of binary tree is"
+printPreorder(root)
+
+print
+"\nInorder traversal of binary tree is"
+printInorder(root)
+
+print
+"\nPostorder traversal of binary tree is"
+printPostorder(root)
