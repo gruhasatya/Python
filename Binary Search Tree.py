07abd97e1bebd26451afa77b2f6a31fc6e26b2a4
gruhasatya <66037927+gruhasatya@users.noreply.github.com>
2022-06-10 12:04:45 +0530

    commit

---
+++ Binary Search Tree.py
@@ -0,0 +1,40 @@
+# Searching
+
+def search(root, key):
+
+    if root is None or root.val == key:
+        return root
+
+    if root.val < key:
+        return search(root.right, key)
+
+    return search(root.left,key)
+
+
+
+# insertion
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
+        else:
+            root.left = insert(root.left, key)
+    return root
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
